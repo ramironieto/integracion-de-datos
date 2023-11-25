d02e31b2d96f587074c73449e1987149a74000f5
Ramiro Nieto <rnieto@192.168.1.7>
2023-11-25 14:43:47 -0300

    scrapper to selenium

--- integracion_de_datos/datos_integrados/management/commands/utils/scrapper.py
+++ integracion_de_datos/datos_integrados/management/commands/utils/scrapper.py
@@ -1,27 +1,23 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+
+url = "http://legendas.tv/busca/walking%20dead%20s03e02"
+
 
 def scrape_property_data(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
+    browser = webdriver.Chrome(executable_path='/Users/rnieto/Downloads/chromedriver-mac-arm64/chromedriver')
+    browser.get(url)
 
-        inmueble = {
-            "titulo": soup.find('span', {'class': 'titulo'}).text.strip(),
-            "tipo": soup.find('span', {'class': 'tipo'}).text.strip(),
-            "cuartos": soup.find('span', {'class': 'cuartos'}).text.strip(),
-            "metros_cuadrados": soup.find('span', {'class': 'metros cuadrados'}).text.strip(),
-            "barrio": soup.find('span', {'class': 'barrio'}).text.strip(),
-            "precio": soup.find('span', {'class': 'precio'}).text.strip(),
-            "gastos_comunes": soup.find('span', {'class': 'gastos comunes'}).text.strip(),
-            "direccion": soup.find('span', {'class': 'direccion'}).text.strip(),
-            "url": url,
-        }
-
-        print(inmueble)
-        
-        return inmueble
-    else:
-        print("Failed to fetch the page")
-        return None
+    inmueble = {
+        "titulo": browser.find_element(By.CLASS_NAME, 'titulo').text.strip(),
+        "tipo": browser.find_element(By.CLASS_NAME, 'tipo').text.strip(),
+        "cuartos": browser.find_element(By.CLASS_NAME, 'cuartos').text.strip(),
+        "metros_cuadrados": browser.find_element(By.CLASS_NAME, 'metros_cuadrados').text.strip(),
+        "barrio": browser.find_element(By.CLASS_NAME, 'barrio').text.strip(),
+        "precio": browser.find_element(By.CLASS_NAME, 'precio').text.strip(),
+        "gastos_comunes": browser.find_element(By.CLASS_NAME, 'gastos_comunes').text.strip(),
+        "direccion": browser.find_element(By.CLASS_NAME, 'direccion').text.strip(),
+        "url": url,
+    }
+    return inmueble
 
