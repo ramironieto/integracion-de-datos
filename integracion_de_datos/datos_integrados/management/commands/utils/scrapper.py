76280d9967401b171e152eb8d1b749bf04c0a6d8
Santi Cuadrado <santiago.cuadrado.vilar@fing.edu.uy>
2023-11-25 16:04:44 -0300

    GL scrapper working

--- integracion_de_datos/datos_integrados/management/commands/utils/scrapper.py
+++ integracion_de_datos/datos_integrados/management/commands/utils/scrapper.py
@@ -1,23 +1,38 @@
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 
-url = "http://legendas.tv/busca/walking%20dead%20s03e02"
+url = 'http://legendas.tv/busca/walking%20dead%20s03e02'
 
 
 def scrape_property_data(url):
-    browser = webdriver.Chrome(executable_path='/Users/rnieto/Downloads/chromedriver-mac-arm64/chromedriver')
+    browser = webdriver.Chrome(executable_path='/Users/santi/Documents/facultad/idatos/chromedriver-mac-arm64/chromedriver')
     browser.get(url)
 
     inmueble = {
-        "titulo": browser.find_element(By.CLASS_NAME, 'titulo').text.strip(),
-        "tipo": browser.find_element(By.CLASS_NAME, 'tipo').text.strip(),
-        "cuartos": browser.find_element(By.CLASS_NAME, 'cuartos').text.strip(),
-        "metros_cuadrados": browser.find_element(By.CLASS_NAME, 'metros_cuadrados').text.strip(),
-        "barrio": browser.find_element(By.CLASS_NAME, 'barrio').text.strip(),
-        "precio": browser.find_element(By.CLASS_NAME, 'precio').text.strip(),
-        "gastos_comunes": browser.find_element(By.CLASS_NAME, 'gastos_comunes').text.strip(),
-        "direccion": browser.find_element(By.CLASS_NAME, 'direccion').text.strip(),
-        "url": url,
+        'titulo': browser.find_element(By.CLASS_NAME, 'titulo').text.strip(),
+        'tipo': find_label_by_icon(browser, 'fa-building'),
+        'cuartos': find_label_by_icon(browser, 'fa-bed'),
+        'metros_cuadrados': find_in_caracteristicas(browser, 'Sup. Construida'),
+        'barrio': find_label_by_icon(browser, 'fa-map-marked'),
+        'precio': browser.find_element(By.CLASS_NAME, 'precio').text.strip(),
+        'gastos_comunes': find_in_caracteristicas(browser, 'Gastos comunes'),
+        'direccion': browser.find_element(By.CLASS_NAME, 'direccion').text.strip(),
+        'url': url,
     }
     return inmueble
 
+def find_label_by_icon(browser, icon):
+    try:
+        icon_selector = f'//i[contains(@class, \'{icon}\')]'
+        icono = browser.find_element(By.XPATH, icon_selector)
+        return icono.find_element(By.XPATH, './ancestor::div[@class=\'wrapperDatos\']/p').text.strip()
+    except:
+        return None
+    
+def find_in_caracteristicas(browser, caracteristica):
+    try:
+        ul_caracteristicas = browser.find_element(By.ID, 'ul_caracteristicas')
+        caracteristica_selector = f'//li[contains(text(), \'{caracteristica}\')]'
+        return ul_caracteristicas.find_element(By.XPATH, caracteristica_selector).text.strip()
+    except:
+        return None
